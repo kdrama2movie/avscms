70b1f5d704543c74cf57bd87c2ff132751b77451
kdrama2movie <157871181+kdrama2movie@users.noreply.github.com>
2025-08-06 15:02:35 +0600

    Update config.db.php

--- include/config.db.php
+++ include/config.db.php
@@ -1,8 +1,9 @@
 <?php
 defined('_VALID') or die('Restricted Access!');
 $config['db_type'] = 'mysqli';
-$config['db_host'] = 'localhost';
-$config['db_user'] = '';
-$config['db_pass'] = '';
-$config['db_name'] = '';
+$config['db_host'] = 'sql12.freesqldatabase.com';
+$config['db_user'] = 'sql12793817';
+$config['db_pass'] = 'Lvz9R8Emgn';
+$config['db_name'] = 'sql12793817';
 ?>
+

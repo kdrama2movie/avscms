b4753b26a6993921f1dfca36d3e2dc8021c94c3c
kdrama2movie <157871181+kdrama2movie@users.noreply.github.com>
2025-08-06 19:26:30 +0600

    Update logout.php

--- siteadmin/logout.php
+++ siteadmin/logout.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('session.save_path', '/tmp');
+session_start();
 define('_VALID', true);
 define('_ADMIN', true);
 require('../include/config.php');
@@ -12,3 +14,4 @@ session_write_close();
 header('Location: index.php');
 die();
 ?>
+

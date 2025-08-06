97693b1183b924ad0ff9f999920e2553f68fc445
kdrama2movie <157871181+kdrama2movie@users.noreply.github.com>
2025-08-06 19:23:19 +0600

    Update view_notice.php

--- siteadmin/view_notice.php
+++ siteadmin/view_notice.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('session.save_path', '/tmp');
+session_start();
 define('_VALID', true);
 define('_ADMIN', true);
 require '../include/config.php';
@@ -26,3 +28,4 @@ if ( $conn->Affected_Rows() == 1 ) {
 $smarty->assign('notice', $notice);
 $smarty->display('view_notice.tpl');
 ?>
+

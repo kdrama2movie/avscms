07f7bafac770199bdf2e173042bb584603b6e038
kdrama2movie <157871181+kdrama2movie@users.noreply.github.com>
2025-08-06 19:27:40 +0600

    Update albums.php

--- siteadmin/albums.php
+++ siteadmin/albums.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('session.save_path', '/tmp');
+session_start();
 define('_VALID', true);
 define('_ADMIN', true);
 require '../include/config.php';
@@ -74,3 +76,4 @@ $smarty->display('leftmenu/menu.tpl');
 $smarty->display($module_template);
 $smarty->display('footer.tpl');
 ?>
+

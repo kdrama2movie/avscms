48a11f8a1f421566d2ac4d08cb9431816b45bd77
kdrama2movie <157871181+kdrama2movie@users.noreply.github.com>
2025-08-06 19:30:25 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('session.save_path', '/tmp');
+session_start();
 define('_VALID', true);
 require 'include/config.php';
 require 'include/function_global.php';
@@ -38,3 +40,4 @@ $smarty->display('header.tpl');
 $smarty->display('index.tpl');
 $smarty->display('footer.tpl');
 ?>
+
